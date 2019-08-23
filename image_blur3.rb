7b8612a8ad017e48a10756895577c0b36e55fe81
Ron <rhon.ventura@gmail.com>
2019-08-23 01:57:35 +0000

    image blur update

--- image_blur3.rb
+++ image_blur3.rb
@@ -6,27 +6,24 @@ class Image
   end
 
   def blur(distance)
-    changed = Array.new
-    
-    @image.each_index do |y|
-      array2 = @image[y]
-      array2.each_index do |x|
-        if array2[x] == 1
-          changed.push([x, y])
+    distance.times do |n|
+      changed = Array.new
+      @image.each_index do |y|
+        array2 = @image[y]
+        array2.each_index do |x|
+          if array2[x] == 1
+            changed.push([x, y])
+          end
         end
       end
-    end
-    
-    changed.each do |pixel|
-      x = pixel[0]
-      y = pixel[1]
-
-      distance.times do |d|
-        base = d+1
-        @image[y][x-base] = 1 unless x-base < 0
-        @image[y][x+base] = 1 unless x+base >= @image[y].length
-        @image[y-base][x] = 1 unless y-base < 0
-        @image[y+base][x] = 1 unless y+base >= @image.length
+      
+      changed.each do |pixel|
+        x = pixel[0]
+        y = pixel[1]
+        @image[y][x-1] = 1 unless x-1 < 0
+        @image[y][x+1] = 1 unless x+1 >= @image[y].length
+        @image[y-1][x] = 1 unless y-1 < 0
+        @image[y+1][x] = 1 unless y+1 >= @image.length
       end
     end
     return @image
@@ -34,18 +31,18 @@ class Image
   
 end
 
-image = Image.new([[0,0,0,0,0,0,0,0,0,0],
+image = Image.new([[0,0,0,0,0,0,0,0,0,1],
+                   [0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0],
                    [0,0,0,1,0,0,0,0,0,0],
                    [0,0,0,0,0,0,0,0,0,0],
                    [0,0,0,0,0,0,0,0,0,0],
                    [0,0,0,0,0,0,0,0,0,0],
                    [0,0,0,0,0,0,0,0,0,0],
-                   [1,0,0,1,0,0,0,0,0,0],
-                   [0,0,0,0,0,0,0,0,0,0],
-                   [0,0,0,0,0,0,0,0,0,0],
-                   [0,0,0,0,0,0,0,0,0,0]])
+                   [0,0,0,0,0,0,0,0,0,1]])
 
-result_image = image.blur(5)
+result_image = image.blur(3)
 
 result_image.each do |n|
     puts n.to_s
